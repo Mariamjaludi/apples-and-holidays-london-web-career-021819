db52e4aaab489883348d970c06c80ab988e544f8
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-10 20:28:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,6 +89,7 @@ def all_supplies_in_holidays(holiday_hash)
           if j == v2.length -1
             str3 = str3 + "#{v2[j]}"
           end
+          puts str3
         end
         else
               puts "#{str2}: #{v2}"
